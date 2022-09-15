86ee120697e76e7340037c037cc7b0fae7ccd6d4
muraviev-aa <muraviev-aa@yandex.ru>
2022-09-15 10:03:17 +0300

    15.09.2022

---
+++ Lab4/Exercise1/ITMO.ADO.NET.Course2022/ITMO.ADO.NET.Course2022.Lab4.Exercise1/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace ITMO.ADO.NET.Course2022.Lab4.Exercise1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.GetCustomersButton = new System.Windows.Forms.Button();
+            this.CustomerListBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // GetCustomersButton
+            // 
+            this.GetCustomersButton.Location = new System.Drawing.Point(53, 24);
+            this.GetCustomersButton.Name = "GetCustomersButton";
+            this.GetCustomersButton.Size = new System.Drawing.Size(130, 40);
+            this.GetCustomersButton.TabIndex = 0;
+            this.GetCustomersButton.Text = "Get Customer";
+            this.GetCustomersButton.UseVisualStyleBackColor = true;
+            this.GetCustomersButton.Click += new System.EventHandler(this.GetCustomersButton_Click);
+            // 
+            // CustomerListBox
+            // 
+            this.CustomerListBox.FormattingEnabled = true;
+            this.CustomerListBox.Location = new System.Drawing.Point(1, 88);
+            this.CustomerListBox.Name = "CustomerListBox";
+            this.CustomerListBox.Size = new System.Drawing.Size(235, 290);
+            this.CustomerListBox.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(238, 384);
+            this.Controls.Add(this.CustomerListBox);
+            this.Controls.Add(this.GetCustomersButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button GetCustomersButton;
+        private System.Windows.Forms.ListBox CustomerListBox;
+    }
+}
+
---
+++ Lab4/Exercise1/ITMO.ADO.NET.Course2022/ITMO.ADO.NET.Course2022.Lab4.Exercise1/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ITMO.ADO.NET.Course2022.Lab4.Exercise1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void GetCustomersButton_Click(object sender, EventArgs e)
+        {
+            NorthwindDataSet northwindDataSet1 = new NorthwindDataSet();
+            NorthwindDataSetTableAdapters.CustomersTableAdapter
+            customersTableAdapter1 = new NorthwindDataSetTableAdapters.CustomersTableAdapter();
+            customersTableAdapter1.Fill(northwindDataSet1.Customers);
+
+            foreach (NorthwindDataSet.CustomersRow nWCustomer in northwindDataSet1.Customers.Rows)
+            {
+                CustomerListBox.Items.Add(nWCustomer.CompanyName);
+            }
+        }
+    }
+}
